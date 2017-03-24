c032a98fe5b0d668b1266552783fb2b07b05cd62
JasmineDeng <jasmine.deng@gmail.com>
2017-03-23 18:00:10 -0700

    moved cheetah and stacker to new files, created stacker environment

--- envs/move_agent.py
+++ envs/move_agent.py
@@ -3,6 +3,8 @@ import numpy as np
 from overrides import overrides
 from rllab.mujoco_py import MjModel
 
+from cheetah_agent import HalfCheetahSimulator, CheetahIm, RewardCheetah
+from stacker_agent import SimpleStackerSimulator, StackerIm, RewardStacker
 from pyhelper_fns import vis_utils
 
 def str2action(cmd):
@@ -154,40 +156,6 @@ class MoveTeleportSimulator(BaseSimulator):
   def render(self):
     self._canvas._display(self.get_image())
 
-class HalfCheetahSimulator(MoveTeleportSimulator):
-  def __init__(self, **kwargs):
-    super(HalfCheetahSimulator, self).__init__(**kwargs)
-    self.model = MjModel('rlmaster/envs/mujoco_envs/xmls/half_cheetah.xml')
-    self._pos = {}
-    self._pos['torso'] = np.zeros((3,))
-    self._range_min = -1
-    self._range_max = 1
-
-    self._imSz = 32
-    self._im = np.zeros((self._imSz, self._imSz, 3), dtype=np.uint8)
-    self.body_comvel = 0
-    self.action = np.zeros((1, 2))
-
-  @overrides
-  def step(self, ctrl):
-    self.model.data.ctrl = ctrl
-    for i in range(10):
-      self.model.step()
-    self.model.forward()
-    ind = self.model.body_names.index('torso')
-    self._pos['torso'] = self.model.body_pos[ind]
-    self.body_comvel = self.model.body_comvels[ind]
-    self.action = ctrl
-
-  @overrides 
-  def get_image(self):
-    imSz = self._imSz
-    rng = np.linspace(self._range_min, self._range_max, imSz)
-    g_x, g_y = self._get_bin(rng, self._pos['torso'])
-    self._im = np.zeros((imSz, imSz, 3), dtype=np.uint8)   
-    self._plot_object((g_x, g_y), 'r')
-    return self._im.copy()
-  
 class InitFixed(BaseInitializer):
   @overrides
   def sample_env_init(self):
@@ -203,7 +171,6 @@ class InitRandom(BaseInitializer):
     for k in self.simulator._pos.keys():
       self.simulator._pos[k] = range_mag * self.random.rand(2,) + \
                                self.simulator._range_min
-
 
 class ObsState(BaseObservation):
   @overrides
@@ -234,14 +201,6 @@ class ObsIm(BaseObservation):
     obs['im'] =  self.simulator.get_image()
     return obs
 
-class CheetahIm(ObsIm):
-  @overrides
-  def observation(self):
-    obs = {}
-    obs['im'] = self.simulator.get_image().flatten()
-    return obs
-
-
 class RewardSimple(BaseRewarder):
   #The radius around the goal in which reward is provided to the agent.
   @property
@@ -255,22 +214,6 @@ class RewardSimple(BaseRewarder):
     else:
       return 0 
 
-class RewardCheetah(BaseRewarder):
-
-  @property
-  def action(self):
-    return self.prms['sim'].action if hasattr(self.prms['sim'], 'action') else np.zeros((1,2))
-
-  @property
-  def body_comvel(self):
-    return self.prms['sim'].body_comvel if hasattr(self.prms['sim'], 'body_comvel') else 0
-
-  @overrides
-  def get(self):
-    ctrl_cost = 1e-1 * 0.5 * np.sum(np.square(self.action))
-    run_cost = -1 * self.body_comvel[0]
-    return ctrl_cost + run_cost
- 
 
 def get_environment(initName='InitRandom', obsName='ObsIm', rewName='RewardSimple',
                     actType='DiscreteActionFour', simName='MoveTeleportSimulator',
---
+++ envs/stacker_agent.py
@@ -0,0 +1,84 @@
+from move_agent import MoveTeleportSimulator, ObsIm, BaseRewarder
+
+class StackedBox():
+
+  def __init__(self, pos, width=3, height=3, length=3):
+    assert len(pos) == 3, 'must enter an xyz position'
+
+    self.pos = pos
+    self.width = width 
+    self.height = height
+    self.length = length
+
+  def contains(self, new_pos):
+    x, y, z = self.pos
+    new_x, new_y, new_z = new_pos
+    return new_x >= x and new_x <= x + self.width \
+      and new_y >= y and new_y <= y + self.height and new_z >= z and new_z <= z + self.length
+
+class SimpleStackerSimulator(MoveTeleportSimulator):
+  def __init__(self, **kwargs):
+    super(SimpleStackerSimulator, self).__init__(**kwargs)
+    self.width = self._imSz
+    self.height = self._imSz
+    self.block1 = StackedBox(np.random.randint(0, 32 - 3, size=3))
+    self.block1.pos[2] = 0
+    self.block2 = StackedBox(np.random.randint(0, 32 - 3, size=3))
+    self.block2.pos[2] = 0
+    if self.block1.contains(self.block2.pos):
+      self.block2.pos[2] = self.block1.height
+
+    self._imSz = 32
+    self._im = np.zeros((self._imSz, self._imSz, 3), dtype=np.uint8)
+    self._range_min = 0
+    self._range_max = 32 - self.block1.width
+
+  @overrides
+  def step(self, pos):
+    assert len(pos) == 2, 'step takes in an xy position'
+
+    x, y = min(pos[0], 32 - 3), min(pos[1], 32 - 3)
+    self.block2.pos = np.array([x, y, 0])
+    if self.block1.contains(self.block2.pos):
+      self.block2.pos[2] = self.block1.height
+
+  @overrides
+  def _plot_object(self, coords, color='r'):
+    x, y = coords
+    mnx, mxx  = x, min(self._imSz, x + self.block1.width)
+    mny, mxy  = y, min(self._imSz, y + self.block1.height)
+    if color == 'r':
+      self._im[mny:mxy, mnx:mxx, 0] = 255
+    elif color == 'g':
+      self._im[mny:mxy, mnx:mxx, 1] = 255
+    else:
+      self._im[mny:mxy, mnx:mxx, 2] = 255
+
+  @overrides
+  def get_image(self):
+    imSz = self._imSz
+    rng = np.linspace(self._range_min, self._range_max, imSz)
+    x_1, y_1 = self.block1.pos[0], self.block1.pos[1]
+    x_2, y_2 = self.block2.pos[0], self.block2.pos[1]
+    self._im = np.zeros((imSz, imSz, 3), dtype=np.uint8)
+    self._plot_object((x_1, y_1), 'r')
+    self._plot_object((x_2, y_2), 'g')
+    return self._im.copy()
+
+class StackerIm(ObsIm):
+  
+  @overrides
+  def observation(self):
+    obs = {}
+    obs['im'] = self.simulator.get_image().flatten()
+    return obs
+
+class RewardStacker(BaseRewarder):
+  
+  @property
+  def block_height(self):
+    return self.prms['sim'].block2.pos[2] if hasattr(self.prms['sim'], 'block2') else 0
+
+  @overrides
+  def get(self):
+    return self.block_height
